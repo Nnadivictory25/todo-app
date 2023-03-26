42a0c0728c6c7e205d023d01af0576e19348d85b
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-26 19:41:45 +0100

    update Readme file

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -14,12 +14,12 @@ const TodoInput = ({ onSubmit, theme }: Props) => {
     
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
-        inputRef.current && onSubmit(inputRef.current.value)
+        inputRef.current?.value && onSubmit(inputRef.current.value)
         formRef.current?.reset();
     }
     return (
         <form ref={formRef} className={`rounded shadow-sm relative transition duration-150 bg-${themeColor}`} onSubmit={handleSubmit}>
-            <input ref={inputRef} type="text" className="w-full outline-none p-4 pl-16 bg-transparent" placeholder="Create a new todo..." maxLength={60}/>
+            <input ref={inputRef} type="text" className="w-full outline-none p-4 pl-16 bg-transparent" placeholder="Create a new todo..." maxLength={60} minLength={3} />
             <div className={`circle w-6 h-6 border rounded-full border-${themeColor} absolute top-[26%] left-5`}></div>
         </form>
     );
